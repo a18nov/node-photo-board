cc539bb7e4ab9a18b686de1c1367a03413f2bab0
Ankit <a18nov@gmail.com>
2019-05-10 01:44:40 +0530

    user model

--- src/models/user.js
+++ src/models/user.js
@@ -6,13 +6,38 @@ var User = mongoose.model('User',{
         required: true,
         minlength: 3,
         trim: true
-    },email: {
+    }, email: {
         type: String,
         required: true,
         minlength: 3,
         trim: true,
         unique: true,
+    }, display_name: {
+        type: String,
+        trim: true
+    }, description: {
+        type: String
+    }, profile_image_url: {
+        type: URL
+    }, phone_number: {
+        type: Number
     }
 });
+
+class UserClass {
+    static async createUser(name, email, display_name, description, profile_image_url, phone_number){
+        let user = new User({
+            name: name,
+            email: email,
+            display_name: display_name,
+            description: description,
+            profile_image_url: profile_image_url,
+            phone_number: phone_number
+        });
+
+        await user.save();
+        return user;
+    }
+}
 
 module.exports = {User};
