8fe5c85fa47828c1f0f73b55c4e9b4b49af4a8fa
Ankit Srivastava <a18nov@gmail.com>
2019-05-16 15:44:12 +0530

    follow refactoring

--- src/routes/follow.js
+++ src/routes/follow.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const { FollowModel } = require('../models/Follow');
+const { UserModel } = require('../models/User');
 router.use(express.json());
 
-router.post('/following', async(req, res) => {
+router.post('/:id', async(req, res) => {
 
     let follow = {
         userId: req.body.userId,
-        userIdFollowed: req.body.userIdFollowed
+        user_to_follow: req.params.id
     }
 
     var followed = true;
-    FollowModel.findOneAndUpdate({
-        userId: follow.userId
-    },
-    { $push:{'userFollowing': {userId: follow.userIdFollowed}}},
-        {new: true, upsert: true},
-        function(err, model){
+    FollowModel.findOneAndUpdate({userId: follow.userId},
+    {$push:{'userFollowing': {userId: follow.user_to_follow}}},
+        {new: true, upsert: true},(err, model) => {
             console.log(err);
             followed = false;
         }
     )
 
-    FollowModel.findOneAndUpdate({
-        userId: follow.userIdFollowed
-    },
-    { $push:{'userFollowedBy': {userId: follow.userId}}},
-        {new: true, upsert: true},
-        function(err, model){
+    FollowModel.findOneAndUpdate({userId: follow.user_to_follow},
+    {$push:{'userFollowedBy': {userId: follow.userId}}},
+        {new: true, upsert: true}, (err, model) => {
             console.log(err);
             followed = false;
         }
@@ -38,6 +33,14 @@ router.post('/following', async(req, res) => {
     } else{
         res.status(400).send('Unable to follow xD');
     }
+});
+
+router.get('/followers/:id', async(req, res) => {
+    let user_id = req.params.id;
+    let user_list = await FollowModel.findOne({userId: user_id});
+    return res.send({
+        user_list: user_list.userFollowedBy
+    });
 });
 
 module.exports = router;
